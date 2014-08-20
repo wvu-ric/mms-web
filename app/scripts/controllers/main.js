e89599599400ace8fda68526e0a1f651fd407172
Sam McLaughlin <sammclaughlin@flocoapps.com>
2014-08-19 21:36:22 -0400

    Added map

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,6 +3,20 @@
 angular.module('mms')
   .controller('MainCtrl', function ($scope) {
 
+    $scope.map = {
+      center: {
+        latitude: 45,
+        longitude: -73
+      },
+      zoom: 8,
+      options:{
+        disableDefaultUI:true
+      }
+    };
+
+    $scope.items = [];
+
+
   });
 
 'use strict';

58a3ab292db32c7aedec589be856f62ab0f74016
Sam McLaughlin <sammclaughlin@flocoapps.com>
2014-08-20 02:04:43 -0400

    Back button works from member page

--- app/scripts/directives/menu/mms-menu.js
+++ app/scripts/directives/menu/mms-menu.js
@@ -12,12 +12,40 @@ angular.module('mms.components')
       templateUrl:'/scripts/directives/menu/mms-menu.html',
       link: function postLink(scope, element, attrs) {
       },
-      controller:function($scope, $log){
+      controller:function($scope, $log, $rootScope, $route, $location){
         $scope.menuOpen = false;
         $scope.toggleMenu = function(){
           $log.debug('mms.components - Menu toggled');
           $scope.menuOpen = !$scope.menuOpen;
+        };
+
+        function checkRoute(){
+          console.log($route.current);
+          if($route.current.loadedTemplateUrl == "views/main.html"){
+            $log.debug('mms.components - Root page');
+            $scope.isChildPage = false;
+          }
+          else{
+            $log.debug('mms.components - Child page');
+            $scope.isChildPage = true;
+          }
         }
+
+        $scope.back = function(){
+          $location.path( "/" );
+        };
+
+        $rootScope.$on('$locationChangeSuccess', function() {
+          checkRoute();
+        });
+
+        $rootScope.$on('$routeChangeSuccess', function() {
+          checkRoute();
+        });
+
+
+        checkRoute();
+
       }
     };
   });

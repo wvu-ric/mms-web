3781d12172a0476a4527c5c9d79ac196ea757bc4
Sam McLaughlin <sammclaughlin@flocoapps.com>
2014-10-02 12:18:55 -0400

    Added admin app folder

---
+++ app/admin/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mmsadminApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mmsadminApp
+ */
+angular.module('mmsadminApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/admin/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mmsadminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mmsadminApp
+ */
+angular.module('mmsadminApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
